a5be5203d74b0f3621dbdd4727b536e6f1008c6a
Raphael <89421168+blocen@users.noreply.github.com>
2022-01-12 00:02:15 +0100

    basic setup

--- public/app.js
+++ public/app.js
@@ -1,4 +1,8 @@
-console.log(firebase);
-console.log(app);
-console.log("something...");
+// import { getApp } from 'firebase/app';
+// // Return the default app
+// const app = getApp();
+// console.log(app);
 
+
+// console.log(app);
+console.log("something...");
